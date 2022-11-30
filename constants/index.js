04a4a745ea9f81d86ec9570c65516b4052aea7a1
chris abdo <chris.j.abdo@gmail.com>
2022-11-29 22:11:35 -0500

    ADDED: professional experience done

--- constants/index.js
+++ constants/index.js
@@ -1,109 +1,109 @@
 export const exploreWorlds = [
   {
-    id: "world-1",
-    imgUrl: "/soulmates.png",
-    title: "Soulmates",
+    id: 'world-1',
+    imgUrl: '/soulmates.png',
+    title: 'Soulmates',
     description:
-      "Soulmates is a soulbound token (SBT) issuance engine, built on Filecoin Virtual Machine. Proud to be the Grand Prize winner of HackFEVM 2022!",
-    link: "https://ethglobal.com/showcase/soulmates-wveqg",
-    imageUrl: "/sun.png",
+      'Soulmates is a soulbound token (SBT) issuance engine, built on Filecoin Virtual Machine. Proud to be the Grand Prize winner of HackFEVM 2022!',
+    link: 'https://ethglobal.com/showcase/soulmates-wveqg',
+    imageUrl: '/sun.png',
   },
   {
-    id: "world-2",
-    imgUrl: "/doodl.png",
-    title: "Doodl",
+    id: 'world-2',
+    imgUrl: '/doodl.png',
+    title: 'Doodl',
     description:
-      "Doodl is a community art game built on Polygon. You are given a daily prompt, and have 24 hours to draw your interpretation of it using a built-in drawing tool. There is a voting system to determine the best drawing of the day, and the winner gets a prize!",
-    link: "https://github.com/ChrisAbdo/Doodl-Official",
-    imageUrl: "/doodllogo.png",
+      'Doodl is a community art game built on Polygon. You are given a daily prompt, and have 24 hours to draw your interpretation of it using a built-in drawing tool. There is a voting system to determine the best drawing of the day, and the winner gets a prize!',
+    link: 'https://github.com/ChrisAbdo/Doodl-Official',
+    imageUrl: '/doodllogo.png',
   },
   {
-    id: "world-3",
-    imgUrl: "/destemr.png",
-    title: "DeStemr",
+    id: 'world-3',
+    imgUrl: '/destemr.png',
+    title: 'DeStemr',
     description:
-      "DeStemr is a decentralized NFT marketplace designed specifically for music stems. It is built on the Polygon network, and uses IPFS to store the stems. Users can upload their stems as an NFT and let other producers purchase and earn royalties off the stems.",
-    link: "https://ethglobal.com/showcase/destemr-y5mw7",
-    imageUrl: "/destemrimg.jpg",
+      'DeStemr is a decentralized NFT marketplace designed specifically for music stems. It is built on the Polygon network, and uses IPFS to store the stems. Users can upload their stems as an NFT and let other producers purchase and earn royalties off the stems.',
+    link: 'https://ethglobal.com/showcase/destemr-y5mw7',
+    imageUrl: '/destemrimg.jpg',
   },
   {
-    id: "world-4",
-    imgUrl: "/safe.png",
-    title: "SafeConnect",
+    id: 'world-4',
+    imgUrl: '/safe.png',
+    title: 'SafeConnect',
     description:
-      "I was a member of the SafeConnect team at ETH New York City. SafeConnect is a security protocol built into WalletConnect that alerts users of malicious wallet connections. It is built by using a database of secure websites, and comparing the current website to the database. If the website is not in the database, the user is alerted.",
-    link: "https://github.com/ETH-NYC-Hackathon",
-    imageUrl: "/safe.png",
+      'I was a member of the SafeConnect team at ETH New York City. SafeConnect is a security protocol built into WalletConnect that alerts users of malicious wallet connections. It is built by using a database of secure websites, and comparing the current website to the database. If the website is not in the database, the user is alerted.',
+    link: 'https://github.com/ETH-NYC-Hackathon',
+    imageUrl: '/safe.png',
   },
   {
-    id: "world-5",
-    imgUrl: "/next.png",
-    title: "Next.js 13 Template",
+    id: 'world-5',
+    imgUrl: '/next.png',
+    title: 'Next.js 13 Template',
     description:
-      "This is a starter template for Next.js 13 to get up and running with the brand new app directory in no time. It comes with an example utilizing a search and pull feature.",
-    link: "https://github.com/ChrisAbdo/Nextjs13-StartingTemplate",
-    imageUrl: "/nextjs.png",
+      'This is a starter template for Next.js 13 to get up and running with the brand new app directory in no time. It comes with an example utilizing a search and pull feature.',
+    link: 'https://github.com/ChrisAbdo/Nextjs13-StartingTemplate',
+    imageUrl: '/nextjs.png',
   },
 ];
 
 export const startingFeatures = [
-  "Find a world that suits you and you want to enter",
-  "Enter the world by reading basmalah to be safe",
-  "No need to beat around the bush, just stay on the gas and have fun",
-  "You can also create your own world and invite your friends to join",
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+  'You can also create your own world and invite your friends to join',
 ];
 
 export const newFeatures = [
   {
-    imgUrl: "/vrpano.svg",
-    title: "A new world",
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
     subtitle:
-      "we have the latest update with new world for you to try never mind",
+      'we have the latest update with new world for you to try never mind',
   },
   {
-    imgUrl: "/headset.svg",
-    title: "More realistic",
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
     subtitle:
-      "In the latest update, your eyes are narrow, making the world more realistic than ever",
+      'In the latest update, your eyes are narrow, making the world more realistic than ever',
   },
 ];
 
 export const insights = [
   {
-    imgUrl: "/planet-06.png",
-    title: "The launch of the Metaverse makes Elon musk ketar-ketir",
+    imgUrl: '/bitgo.png',
+    title: 'BitGo | Software Engineer Intern',
     subtitle:
-      "Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.",
+      'Assisted in the development of an NFT wallet. Programmed in React, TypeScript, and Solidity. Worked with a team of 5 engineers to quickly and efficiently have a great functional product.',
   },
   {
-    imgUrl: "/planet-07.png",
-    title: "7 tips to easily master the madness of the Metaverse",
+    imgUrl: '/njitt.png',
+    title: 'New Jersey Institute of Technology | SWE Intern',
     subtitle:
-      "Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum",
+      'Developed a neural network simulating the C. elegans nervous system using a Raspberry Pi autonomous vehicle. Programmed in Python and MATLAB for statistical analysis. Worked with another intern to over the course of 6 weeks to complete the project.',
   },
   {
-    imgUrl: "/planet-08.png",
-    title: "With one platform you can explore the whole world virtually",
+    imgUrl: '/leb.png',
+    title: 'Lebanese Forces | Software Engineering & Data',
     subtitle:
-      "Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem",
+      'Assisted in the development of a web application for the Lebanese Forces to allow overseas voters to cast their vote. Programmed in React, TypeScript, and MongoDB.This application verified the voter’s identity and allowed them to cast their vote.',
   },
 ];
 
 export const socials = [
   {
-    name: "twitter",
-    url: "/twitter.svg",
+    name: 'twitter',
+    url: '/twitter.svg',
   },
   {
-    name: "linkedin",
-    url: "/linkedin.svg",
+    name: 'linkedin',
+    url: '/linkedin.svg',
   },
   {
-    name: "instagram",
-    url: "/instagram.svg",
+    name: 'instagram',
+    url: '/instagram.svg',
   },
   {
-    name: "facebook",
-    url: "/facebook.svg",
+    name: 'facebook',
+    url: '/facebook.svg',
   },
 ];
